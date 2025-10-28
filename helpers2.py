d72a08c8e8ccd03ae208c81a2bd4538fa93461a0
devwithnaveed <49093438+devwithnaveed@users.noreply.github.com>
2025-10-28 19:16:50 +0500

    Update getData function to reduce data options
    Removed 'date' and 'elderberry' from data list.

--- helpers2.py
+++ helpers2.py
@@ -1,6 +1,6 @@
 import random
 
 def getData():
-    data = ["apple", "banana", "cherry", "date", "elderberry"]
+    data = ["apple", "banana", "cherry"]
     idx = random.randint(0, len(data)-1)
     return data[idx]
